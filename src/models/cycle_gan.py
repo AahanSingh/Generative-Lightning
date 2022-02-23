d144742618704b59d900bdcedf4292134ed5fc39
AahanSingh <aahansingh95@gmail.com>
2022-02-23 23:03:12 +0800

    Renamed model.py to cycle_gan.py

--- src/models/cycle_gan.py
+++ src/models/cycle_gan.py
@@ -6,7 +6,8 @@ from torch import nn
 import torch.utils.data
 import pytorch_lightning as pl
 from .losses import discriminator_loss, generator_loss, cycle_loss, identity_loss
-from .utils import downsample, upsample
+from .generators import Generator
+from .discriminators import Discriminator
 
 
 def weights_init(m):
@@ -18,65 +19,6 @@ def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
-
-
-class Generator(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-        self.down_stack = nn.ModuleList([
-            downsample(in_channels=3, out_channels=64, kernel_size=2, apply_instancenorm=False),
-            downsample(in_channels=64, out_channels=128, kernel_size=2),
-            downsample(in_channels=128, out_channels=256, kernel_size=2),
-            downsample(in_channels=256, out_channels=512, kernel_size=2),
-            downsample(in_channels=512, out_channels=512, kernel_size=2),
-            downsample(in_channels=512, out_channels=512, kernel_size=2),
-            downsample(in_channels=512, out_channels=512, kernel_size=2),
-            downsample(in_channels=512, out_channels=512, kernel_size=2, apply_instancenorm=False),
-        ])
-        self.up_stack = nn.ModuleList([
-            upsample(in_channels=512, out_channels=512, kernel_size=2, apply_dropout=True),
-            upsample(in_channels=1024, out_channels=512, kernel_size=2, apply_dropout=True),
-            upsample(in_channels=1024, out_channels=512, kernel_size=2, apply_dropout=True),
-            upsample(in_channels=1024, out_channels=512, kernel_size=2),
-            upsample(in_channels=1024, out_channels=256, kernel_size=2),
-            upsample(in_channels=512, out_channels=128, kernel_size=2),
-            upsample(in_channels=256, out_channels=64, kernel_size=2)
-        ])
-        self.last = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=128, out_channels=3, kernel_size=2, stride=2), nn.Tanh())
-
-    def forward(self, input):
-        # Downsampling through the model
-        x = input
-        skips = []
-        for down in self.down_stack:
-            x = down(x)
-            skips.append(x)
-        skips = reversed(skips[:-1])
-        # Upsampling and establishing the skip connections
-        for up, skip in zip(self.up_stack, skips):
-            x = up(x)
-            x = torch.cat((x, skip), dim=1)
-        x = self.last(x)
-        return x
-
-
-class Discriminator(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-        self.layers = nn.Sequential(
-            downsample(in_channels=3, out_channels=64, kernel_size=2, apply_instancenorm=False),
-            downsample(in_channels=64, out_channels=128, kernel_size=2, apply_instancenorm=False),
-            downsample(in_channels=128, out_channels=256, kernel_size=2, apply_instancenorm=False),
-            nn.ZeroPad2d(1),
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=1, bias=False),
-            nn.InstanceNorm2d(num_features=512), nn.LeakyReLU(), nn.ZeroPad2d(1),
-            nn.Conv2d(in_channels=512, out_channels=1, kernel_size=4, stride=1))
-
-    def forward(self, input):
-        return self.layers(input)
 
 
 class CycleGAN(pl.LightningModule):
