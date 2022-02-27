5b224fbda308a9fd42a3a5423180e71bfcb3e825
AahanSingh <aahansingh95@gmail.com>
2022-02-28 01:02:07 +0800

    Experimental: Combined optimizers for the 2 discriminators and generators into one.

--- lightning_gans/models/cycle_gan.py
+++ lightning_gans/models/cycle_gan.py
@@ -1,5 +1,6 @@
 """Contains model code for the CycleGAN
 """
+import itertools
 import numpy as np
 import torch
 from torch import nn
@@ -50,6 +51,7 @@ class CycleGAN(pl.LightningModule):
         Initializes the optimizer and learning rate scheduler
         :return: output - Initialized optimizer and scheduler
         """
+        """
         self.monet_generator_optimizer = torch.optim.Adam(self.m_gen.parameters(),
                                                           lr=2e-4,
                                                           betas=(0.5, 0.999))
@@ -69,10 +71,72 @@ class CycleGAN(pl.LightningModule):
             self.photo_generator_optimizer,
             self.monet_discriminator_optimizer,
             self.photo_discriminator_optimizer,
-        ]
+        ]"""
+        self.generator_optimizer = torch.optim.Adam(itertools.chain(self.m_gen.parameters(),
+                                                                    self.p_gen.parameters()),
+                                                    lr=2e-4,
+                                                    betas=(0.5, 0.999))
+        self.discriminator_optimizer = torch.optim.Adam(itertools.chain(
+            self.m_disc.parameters(), self.p_disc.parameters()),
+                                                        lr=2e-4,
+                                                        betas=(0.5, 0.999))
+        return [self.generator_optimizer, self.discriminator_optimizer]
 
     def training_step(self, train_batch, batch_idx, optimizer_idx):
         real_monet, real_photo = train_batch
+        if optimizer_idx == 0:
+            # Cycle the Photo
+            fake_monet = self.m_gen(real_photo)
+            cycled_photo = self.p_gen(fake_monet)
+            # monet to photo back to monet
+            fake_photo = self.p_gen(real_monet)
+            cycled_monet = self.m_gen(fake_photo)
+            # discriminator used to check, inputing fake images
+            disc_fake_monet = self.m_disc(fake_monet)
+            # generating itself
+            same_monet = self.m_gen(real_monet)
+            # discriminator used to check, inputing fake images
+            disc_fake_photo = self.p_disc(fake_photo)
+            # generating itself
+            same_photo = self.p_gen(real_photo)
+            # evaluates total cycle consistency loss
+            total_cycle_loss = self.cycle_loss_fn(real_monet, cycled_monet,
+                                                  self.lambda_cycle) + self.cycle_loss_fn(
+                                                      real_photo, cycled_photo, self.lambda_cycle)
+            # evaluates generator loss
+            monet_gen_loss = self.gen_loss_fn(disc_fake_monet)
+            # evaluates total generator loss
+            total_monet_gen_loss = (
+                monet_gen_loss + total_cycle_loss +
+                self.identity_loss_fn(real_monet, same_monet, self.lambda_cycle))
+            # evaluates generator loss
+            photo_gen_loss = self.gen_loss_fn(disc_fake_photo)
+            # evaluates total generator loss
+            total_photo_gen_loss = (
+                photo_gen_loss + total_cycle_loss +
+                self.identity_loss_fn(real_photo, same_photo, self.lambda_cycle))
+            return total_monet_gen_loss + total_photo_gen_loss
+
+        if optimizer_idx == 1:
+            # photo to monet back to photo
+            fake_monet = self.m_gen(real_photo)
+            # discriminator used to check, inputing fake images
+            disc_fake_monet = self.m_disc(fake_monet)
+            # discriminator used to check, inputing real images
+            disc_real_monet = self.m_disc(real_monet)
+
+            # photo to monet back to photo
+            fake_photo = self.p_gen(real_monet)
+            # discriminator used to check, inputing fake images
+            disc_fake_photo = self.p_disc(fake_photo)
+            # discriminator used to check, inputing real images
+            disc_real_photo = self.p_disc(real_photo)
+            # evaluates discriminator loss
+            monet_disc_loss = self.disc_loss_fn(disc_real_monet, disc_fake_monet)
+            # evaluates discriminator loss
+            photo_disc_loss = self.disc_loss_fn(disc_real_photo, disc_fake_photo)
+            return monet_disc_loss + photo_disc_loss
+        """
         if optimizer_idx == 0:
             cycled_photo, cycled_monet, disc_fake_monet, same_monet = self.forward(
                 real_monet, real_photo, idx=optimizer_idx)
@@ -117,9 +181,10 @@ class CycleGAN(pl.LightningModule):
                                                             idx=optimizer_idx)
             # evaluates discriminator loss
             photo_disc_loss = self.disc_loss_fn(disc_real_photo, disc_fake_photo)
-            return photo_disc_loss
+            return photo_disc_loss"""
 
     def forward(self, real_monet, real_photo, idx):
+        """
         if idx == 0:  # Monet Generator
             # Cycle the Photo
             fake_monet = self.m_gen(real_photo)
@@ -162,20 +227,25 @@ class CycleGAN(pl.LightningModule):
             disc_fake_photo = self.p_disc(fake_photo)
             # discriminator used to check, inputing real images
             disc_real_photo = self.p_disc(real_photo)
-            return disc_fake_photo, disc_real_photo
+            return disc_fake_photo, disc_real_photo"""
 
     def backward(self, loss, optimizer, optimizer_idx, *args, **kwargs) -> None:
         loss = loss.mean()
         loss.backward()
 
-    def train_epoch_end(self, losses) -> None:
+    def training_epoch_end(self, outputs) -> None:
         # total_monet_gen_loss, total_photo_gen_loss, monet_disc_loss, photo_disc_loss
-        self.log("train/monet_gen_loss", losses[0].mean(), prog_bar=True)
-        self.log("train/photo_gen_loss", losses[1].mean(), prog_bar=True)
-        self.log("train/monet_disc_loss", losses[2].mean(), prog_bar=True)
-        self.log("train/photo_disc_loss", losses[3].mean(), prog_bar=True)
-        total_loss = 0
-        for loss in losses:
-            total_loss += loss.mean()
-        self.log("train/total_loss", total_loss, prog_bar=True)
+        losses = [0 for _ in range(len(outputs))]
+        for i, output in enumerate(outputs):
+            for loss in output:
+                losses[i] += loss["loss"]
+            losses[i] /= len(output)
+        self.log("train/generator_loss", losses[0], prog_bar=True)
+        self.log("train/discriminator_loss", losses[1], prog_bar=True)
+        """
+        self.log("train/monet_gen_loss", losses[0], prog_bar=True)
+        self.log("train/photo_gen_loss", losses[1], prog_bar=True)
+        self.log("train/monet_disc_loss", losses[2], prog_bar=True)
+        self.log("train/photo_disc_loss", losses[3], prog_bar=True)"""
+        self.log("train/total_loss", sum(losses) / len(outputs), prog_bar=True)
         return
