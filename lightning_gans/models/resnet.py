03a1a4f4cc451c33452b2a8afa3cf29e50fbf044
AahanSingh <aahansingh95@gmail.com>
2022-03-02 23:37:13 +0800

    Improved modularity by removing hardcoded normalizations and activations.

--- lightning_gans/models/resnet.py
+++ lightning_gans/models/resnet.py
@@ -5,34 +5,31 @@ activations = {
     "relu": torch.nn.ReLU(inplace=True),
     "tanh": torch.nn.Tanh()
 }
+norms = {"batch": torch.nn.BatchNorm2d, "instance": torch.nn.InstanceNorm2d}
 
 
-def conv_downsample(ks, st, in_c, out_c, activation="leaky_relu", apply_bn=True):
-    layers = [
-        torch.nn.Conv2d(in_c, out_c, ks, st, padding=ks // 2, bias=False),
-        torch.nn.BatchNorm2d(num_features=out_c)
-    ]
-    if not apply_bn:
-        layers.pop(1)
+def conv_downsample(ks, st, in_c, out_c, activation="leaky_relu", norm="batch"):
+    layers = [torch.nn.Conv2d(in_c, out_c, ks, st, padding=ks // 2, bias=False)]
+    if norm:
+        layers.append(norms[norm](num_features=out_c))
     if activation:
         layers.append(activations[activation])
     return torch.nn.Sequential(*layers)
 
 
-def conv_upsample(ks, st, in_c, out_c, activation="leaky_relu", apply_bn=True):
+def conv_upsample(ks, st, in_c, out_c, activation="leaky_relu", norm="batch"):
     layers = [
         torch.nn.ConvTranspose2d(in_c, out_c, ks, st, padding=ks // 2, output_padding=1,
                                  bias=False),
-        torch.nn.BatchNorm2d(num_features=out_c),
     ]
-    if not apply_bn:
-        layers.pop(1)
+    if norm:
+        layers.append(norms[norm](num_features=out_c))
     if activation:
         layers.append(activations[activation])
     return torch.nn.Sequential(*layers)
 
 
-def wide_block(in_channels, res_channels, l, k, kernel_size):
+def wide_block(in_channels, l, k, kernel_size):
     prev_out_channels = in_channels
     layers = []
     for _ in range(l - 1):
@@ -40,34 +37,34 @@ def wide_block(in_channels, res_channels, l, k, kernel_size):
             conv_downsample(ks=kernel_size,
                             st=1,
                             in_c=prev_out_channels,
-                            out_c=res_channels * k,
-                            activation="leaky_relu"))
-        prev_out_channels = res_channels * k
+                            out_c=in_channels * k,
+                            activation="leaky_relu",
+                            norm="batch"))
+        prev_out_channels = in_channels * k
     layers.append(
         conv_downsample(ks=kernel_size,
                         st=1,
                         in_c=prev_out_channels,
                         out_c=in_channels,
-                        activation=None))
+                        activation=None,
+                        norm=None))
     layers = torch.nn.Sequential(*layers)
     return layers
 
 
 class ResidualLayer(torch.nn.Module):
 
-    def __init__(self, l, k, in_channels, res_channels, kernel_size):
+    def __init__(self, l, k, in_channels, kernel_size, activation="leaky_relu", norm="batch"):
         super().__init__()
-        self.wide_block = wide_block(in_channels=in_channels,
-                                     res_channels=res_channels,
-                                     l=l,
-                                     k=k,
-                                     kernel_size=kernel_size)
-        self.relu = torch.nn.LeakyReLU(inplace=True)
+        self.wide_block = wide_block(in_channels=in_channels, l=l, k=k, kernel_size=kernel_size)
+        self.activation = activations[activation]
+        self.norm = norms[norm](num_features=in_channels)
 
     def forward(self, x):
         x_out = self.wide_block(x)
         x_out.add_(x)
-        x_out = self.relu(x_out)
+        x_out = self.norm(x_out)
+        x_out = self.activation(x_out)
         return x_out
 
 
