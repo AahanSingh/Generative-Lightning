d471ad51874371dcfd9da02e4e47615c9ce44284
AahanSingh <aahansingh95@gmail.com>
2022-03-03 20:14:17 +0800

    Changed dataloader to repeat monets across all photos.

--- lightning_gans/data/dataloader.py
+++ lightning_gans/data/dataloader.py
@@ -1,6 +1,7 @@
 """
 This modules contains all of the pytorch datasets used in the project.
 """
+import itertools
 import glob
 import random
 from torch.utils.data import Dataset
@@ -11,28 +12,23 @@ class MonetDataset(Dataset):
     """Pytorch Dataset Subclass for the Monet Dataset
     """
 
-    def __init__(self, dataroot, transforms=None, shuffle_photos=False, val=False, val_size=20):
+    def __init__(self, dataroot, transforms=None, shuffle=True):
         super().__init__()
-        self.val = val
         self.monets = glob.glob(dataroot + "monet_jpg/*.jpg")
-        photos = glob.glob(dataroot + "photo_jpg/*.jpg")
-        if shuffle_photos:
-            random.shuffle(photos)
-        if not val:
-            self.photos = photos[:len(self.monets)]
-        else:
-            self.photos = photos[:val_size]
+        self.photos = glob.glob(dataroot + "photo_jpg/*.jpg")
+        if shuffle:
+            random.shuffle(self.photos)
+            random.shuffle(self.monets)
         self.transforms = transforms
+        self.aligned_images = list(zip(itertools.cycle(self.monets), self.photos))
 
     def __getitem__(self, idx):
-        monet = torchvision.io.read_image(self.monets[idx]).float()
-        photo = torchvision.io.read_image(self.photos[idx]).float()
+        monet = torchvision.io.read_image(self.aligned_images[idx][0]).float()
+        photo = torchvision.io.read_image(self.aligned_images[idx][1]).float()
         if self.transforms:
             monet = self.transforms(monet)
             photo = self.transforms(photo)
-        if self.val:
-            return photo
         return monet, photo
 
     def __len__(self):
-        return len(self.monets)
+        return len(self.aligned_images)
