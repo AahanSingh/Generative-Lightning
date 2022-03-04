c2d9edf936e36b489d6dff49a36cba6884c48796
AahanSingh <aahansingh95@gmail.com>
2022-03-04 21:48:40 +0800

    Added default transformations & usage of complete dataset.

--- lightning_gans/data/dataloader.py
+++ lightning_gans/data/dataloader.py
@@ -5,29 +5,47 @@ import itertools
 import glob
 import random
 from torch.utils.data import Dataset
-import torchvision.io
+
+import torchvision
+import cv2
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 
 class MonetDataset(Dataset):
     """Pytorch Dataset Subclass for the Monet Dataset
     """
 
-    def __init__(self, dataroot, transforms=None, shuffle=True):
+    def __init__(self, dataroot, transforms=None, shuffle=True, complete_dataset=True):
         super().__init__()
         self.monets = glob.glob(dataroot + "monet_jpg/*.jpg")
         self.photos = glob.glob(dataroot + "photo_jpg/*.jpg")
         if shuffle:
             random.shuffle(self.photos)
             random.shuffle(self.monets)
-        self.transforms = transforms
-        self.aligned_images = list(zip(itertools.cycle(self.monets), self.photos))
+        self.transforms = A.Compose([
+            A.Resize(256, 256),
+            A.Normalize(mean=0, std=1, max_pixel_value=255),
+            ToTensorV2(),
+        ])
+        if not transforms:
+            self.transforms = transforms
+        self.aligned_images = None
+        if complete_dataset:
+            self.aligned_images = list(zip(itertools.cycle(self.monets), self.photos))
+        else:
+            self.aligned_images = list(zip(self.monets, self.photos))
+
+    def _read_image(self, path):
+        image = cv2.imread(path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
 
     def __getitem__(self, idx):
-        monet = torchvision.io.read_image(self.aligned_images[idx][0]).float()
-        photo = torchvision.io.read_image(self.aligned_images[idx][1]).float()
-        if self.transforms:
-            monet = self.transforms(monet)
-            photo = self.transforms(photo)
+        monet = self._read_image(self.aligned_images[idx][0])
+        photo = self._read_image(self.aligned_images[idx][1])
+        monet = self.transforms(image=monet)["image"].float()
+        photo = self.transforms(image=photo)["image"].float()
         return monet, photo
 
     def __len__(self):
