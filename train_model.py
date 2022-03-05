73c2d2c31c99b1ef82544b54442e76f0e12171eb
AahanSingh <aahansingh95@gmail.com>
2022-03-05 23:42:01 +0800

    Added basic config system.

--- train_model.py
+++ train_model.py
@@ -1,7 +1,8 @@
+import os
 from argparse import ArgumentParser
 from asyncio.log import logger
-from distutils.command import check
-import random
+import munch
+import yaml
 import torch
 import torch.nn.parallel
 import torch.utils.data
@@ -9,96 +10,124 @@ import torchvision.transforms as transforms
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
-#from src.visualization.callbacks import WandbImageCallback
-import wandb
 
-from lightning_gans.models.generators import UNETGenerator, WideResnetEncoderDecoder, WideResnetUNET
+import wandb
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from lightning_gans.models.discriminators import Discriminator
+
+from lightning_gans.models.generators import UNETGenerator, WideResnetEncoderDecoder, WideResnetUNET, CustomUNET
 from lightning_gans.models.cycle_gan import CycleGAN
 from lightning_gans.data.dataloader import MonetDataset
 
-# Set random seed for reproducibility
-manualSeed = 999
-#manualSeed = random.randint(1, 10000) # use if you want new results
-print("Random Seed: ", manualSeed)
-random.seed(manualSeed)
-torch.manual_seed(manualSeed)
+augmentations = {
+    "horizontalflip": A.HorizontalFlip(p=0.5),
+    "randomcrop": A.RandomResizedCrop(height=256, width=256, p=0.5),
+}
 
 
 def main(args):
+    with open(args.config_path, "r") as f:
+        config = munch.munchify(yaml.safe_load(f))
+    os.makedirs(
+        "{}/{}/{}".format(
+            config.savedir,
+            config.experiment_name,
+            config.run_name,
+        ),
+        exist_ok=True,
+    )
+
+    if not config.id:
+        config.id = wandb.util.generate_id()
+    with open(
+            "{}/{}/{}/config.yaml".format(
+                config.savedir,
+                config.experiment_name,
+                config.run_name,
+            ),
+            "w",
+    ) as f:
+        yaml.dump(munch.unmunchify(config), f)
     # Root directory for dataset
-    dataroot = args.data_path
+    dataroot = config.data_path
     # Number of workers for dataloader
     workers = 12
     # Batch size during training
-    batch_size = args.bs
+    batch_size = config.batch_size
     # Spatial size of training images. All images will be resized to this
     #   size using a transformer.
     image_size = 256
     # We can use an image folder dataset the way we have it setup.
     # Create the dataset
+    train_transforms = [
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        ToTensorV2(),
+    ]
+    if config.augmentations is not None:
+        tmp = [augmentations[i] for i in config.augmentations]
+        tmp.extend(train_transforms)
+        train_transforms = tmp
+    train_transforms = A.Compose(train_transforms)
     train_dataset = MonetDataset(dataroot=dataroot,
-                                 transforms=transforms.Compose([
-                                     transforms.Resize(image_size),
-                                     transforms.CenterCrop(image_size),
-                                     transforms.Normalize(127.5, 127.5),
-                                 ]))
+                                 transforms=train_transforms,
+                                 shuffle=config.dataset.shuffle,
+                                 complete_dataset=config.dataset.complete)
     # Create the dataloader
     train_dataloader = torch.utils.data.DataLoader(train_dataset,
                                                    batch_size=batch_size,
                                                    num_workers=workers)
-    val_dataset = MonetDataset(dataroot=dataroot,
-                               transforms=transforms.Compose([
-                                   transforms.Resize(image_size),
-                                   transforms.CenterCrop(image_size),
-                                   transforms.Normalize(127.5, 127.5),
-                               ]),
-                               shuffle_photos=True,
-                               val=True,
-                               val_size=20)
-    # Create the dataloader
-    val_dataloader = torch.utils.data.DataLoader(val_dataset,
-                                                 batch_size=batch_size,
-                                                 num_workers=workers)
     # Create the generator
     generators = {
         "UNET": UNETGenerator,
         "Resnet": WideResnetEncoderDecoder,
-        "WideResnetUNET": WideResnetUNET
+        "WideResnetUNET": WideResnetUNET,
+        "CustomUNET": CustomUNET,
     }
-    model = CycleGAN(generator=generators[args.gen], l=args.l, k=args.k)
-    exp_id = args.id
-    if not exp_id:
-        exp_id = wandb.util.generate_id()
+    model = CycleGAN(generator=generators[config.generator],
+                     discriminator=Discriminator,
+                     l=config.l,
+                     k=config.k)
     wandb_logger = WandbLogger(project="Monet CycleGAN",
                                log_model=True,
-                               save_dir="/home/aahan/KAGGLE_EXPERIMENTS/",
-                               name="{}_{}_{}_noAug".format(args.gen, args.l, args.k),
-                               id=exp_id)
+                               save_dir="{}/{}/{}/".format(
+                                   config.savedir,
+                                   config.experiment_name,
+                                   config.run_name,
+                               ),
+                               name="{}-{}".format(config.experiment_name, config.run_name),
+                               id=config.id,
+                               config=munch.unmunchify(config))
     wandb_logger.watch(model)
     checkpoint_callback = ModelCheckpoint(
         save_top_k=1,
         verbose=True,
-        monitor="train/generator_loss",
+        monitor="train/monet_gen_loss",
         mode="min",
-        filename="model-epoch{epoch:02d}-generator_loss{train/generator_loss:.2f}",
+        filename="model-monet_generator_loss{train/monet_gen_loss:.2f}",
         auto_insert_metric_name=False,
-        dirpath="/home/aahan/KAGGLE_EXPERIMENTS/{}_{}_{}_noAug".format(args.gen, args.l, args.k),
+        dirpath="/home/aahan/KAGGLE_EXPERIMENTS/{}/models".format(config.experiment_name),
+        every_n_train_steps=1)
+    trainer = pl.Trainer.from_argparse_args(
+        args,
+        logger=wandb_logger,
+        callbacks=[checkpoint_callback],
+        max_epochs=config.epochs,
+        accumulate_grad_batches=config.accumulate_grad_batches,
+        log_every_n_steps=1,
     )
-    trainer = pl.Trainer.from_argparse_args(args,
-                                            logger=wandb_logger,
-                                            callbacks=[checkpoint_callback])
 
-    trainer.fit(model, train_dataloader=train_dataloader, val_dataloaders=val_dataloader)
+    trainer.fit(model, train_dataloader=train_dataloader, ckpt_path=config.ckpt_path)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--data-path", metavar="FILE", default=None, required=True)
-    parser.add_argument("--bs", default=128, type=int)
-    parser.add_argument("--gen", required=True, type=str)
-    parser.add_argument("--l", required=True, type=int)
-    parser.add_argument("--k", required=True, type=int)
-    parser.add_argument("--id", required=False, type=str, default=None)
+    parser.add_argument(
+        "--config-path",
+        required=True,
+        metavar="FILE",
+        help="Path to the config",
+    )
     parser = pl.Trainer.add_argparse_args(parent_parser=parser)
     args = parser.parse_args()
     main(args)
